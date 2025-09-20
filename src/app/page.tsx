d8951ae77c56dc057abdf0af016301ad3ee5294a
max.shirinzad@gmail.com <max.shirinzad@gmail.com>
2025-09-20 11:40:28 +0330

    Products page first layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Navigation from "./components/Navigation"
 import Footer from "./components/Footer"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navigation />
+      <div className="flex-grow">
+        <Link href="/products" className="text-blue-500">Go to products</Link>
+      </div>
       <Footer />
     </div>
   )
