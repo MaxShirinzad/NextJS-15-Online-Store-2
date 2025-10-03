a407223dd2e498ad72783e78b2f22fdf5f557834
max.shirinzad@gmail.com <max.shirinzad@gmail.com>
2025-10-03 22:52:45 +0330

    Products page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function HomePage() {
                   width={500}
                   height={400}
                   className="rounded-lg drop-shadow-2xl object-contain" />
-                  <div className="absolute -bottom-4 -right-4 bg-[#edae49]" text-white px-4 py-2 rounded-full shadow-lg text-sm font-bold>
+                  <div className="absolute -bottom-4 -right-4 bg-[#edae49] text-white px-4 py-2 rounded-full shadow-lg text-sm font-bold">
                     NEW ARRIVAL
                   </div>
                 </div>
